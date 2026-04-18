1960cde56abf090d868f5025bc044fbdb3303f45
JagritiM123 <mailjagriti12@gmail.com>
2026-04-18 04:21:23 +0000

    modified object.c

--- object.c
+++ object.c
@@ -92,7 +92,6 @@ int object_exists(const ObjectID *id) {
 //
 
 //
-// Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
     // TODO: Implement
     // 1. Determine type string
